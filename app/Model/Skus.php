af0dcc0a06dd5c3f189bea59155149e7e4116486
dongfanfan <13315478388@163.com>
2017-04-25 18:42:06 +0800

    getAllPros changed

--- app/Model/Skus.php
+++ app/Model/Skus.php
@@ -3,16 +3,19 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-
+use DB;
 class Skus extends Model
 {
 	protected $table = 'skus';
     public static function getAllPros($vmId){
-    	$proList = Skus::where('vm_id',$vmId)
-		    			->select('product_id','product_name','tag_id','original_price','retail_price')
+    	$proList = DB::table('skus')
+    					->join('tags','skus.tag_id','=','tags.id')
+    					->where('vm_id',$vmId)
+		    			->select('skus.product_id','skus.product_name','skus.original_price','skus.retail_price','skus.tag_id','tags.tag_name')
 		    			->groupBy('product_id')
-		    			->get()
-		    			->toArray();
+		    			->get();
+
+
 		return $proList;
     }
 }
