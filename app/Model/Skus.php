88e3590633839943ec926ae145ab2051fb6e06ac
dongfanfan <13315478388@163.com>
2017-04-26 14:54:56 +0800

    make complete data of product

--- app/Model/Skus.php
+++ app/Model/Skus.php
@@ -22,15 +22,27 @@ class Skus extends Model
 		    			->select('product_id','product_name','original_price','retail_price','tag_id')
 		    			->groupBy('product_id')
 		    			->get();
-
+		$proInfos = DB::table('products')->get();
 		$tags = DB::table('tags')->get();
 		foreach ($proList as $k => $pro) {
+			// 添加tag_name信息
 			foreach ($tags as $tag) {
 				if($pro->tag_id == $tag->id){
 					$proList[$k]->tag_name = $tag->tag_name;
 				}
 			}
+
+			// 添加exp 和 volume信息
+			foreach ($proInfos as $proInfo) { //拼接商品生存期
+                if($pro->product_id == $proInfo->id){
+                    $proList[$k]->exp = $proInfo->exp;
+                    $unit = ($proInfo->unit==1)?'ml':'g';
+                    $proList[$k]->volume = ((string)$proInfo->volume).$unit;
+                }
+            }
+
 		}
 		return $proList;
     }
+
 }
