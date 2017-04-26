2ee1a681fdb05e6ab8e7c505db20b7e8c00ae88d
dongfanfan <13315478388@163.com>
2017-04-26 09:56:19 +0800

    proData add tag_name

--- app/Model/Skus.php
+++ app/Model/Skus.php
@@ -8,14 +8,29 @@ class Skus extends Model
 {
 	protected $table = 'skus';
     public static function getAllPros($vmId){
+    	/**
     	$proList = DB::table('skus')
     					->join('tags','skus.tag_id','=','tags.id')
     					->where('vm_id',$vmId)
 		    			->select('skus.product_id','skus.product_name','skus.original_price','skus.retail_price','skus.tag_id','tags.tag_name')
 		    			->groupBy('product_id')
 		    			->get();
+		**/
 
+		$proList = DB::table('skus')
+    					->where('vm_id',$vmId)
+		    			->select('product_id','product_name','original_price','retail_price','tag_id')
+		    			->groupBy('product_id')
+		    			->get();
 
+		$tags = DB::table('tags')->get();
+		foreach ($proList as $k => $pro) {
+			foreach ($tags as $tag) {
+				if($pro->tag_id == $tag->id){
+					$proList[$k]->tag_name = $tag->tag_name;
+				}
+			}
+		}
 		return $proList;
     }
 }
