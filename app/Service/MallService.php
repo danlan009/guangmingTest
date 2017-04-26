9e29990fb5b4cf1660fb57aeba72c0973d82a6b8
dongfanfan <13315478388@163.com>
2017-04-26 14:53:20 +0800

    add getNumOfSale/getNumOfBook

--- app/Service/MallService.php
+++ app/Service/MallService.php
@@ -21,42 +21,47 @@ class MallService{
 	}
 
 	// 根据vmid 拉取所有商品
-	// 参数:vmId 售货机id
-    public function showPros($vmId){ 
-        $proList = Skus::getAllPros($vmId);
+	// 参数:1.vmId 售货机id 
+    //      2.type 'sale':即卖
+    //             'book':预定
+
+    public function showPros($vmid , $type){
+        if(!isset($vmid) || !isset($type)){
+            return 'parameter missing!';
+        }
+
+        $proList = Skus::getAllPros($vmid);
         Log::debug('MallService::getAllPros---'.json_encode($proList));
-        $exps = DB::table('products')->get();
-        $tags = DB::table('tags')->get();
+
         // 放入缓存
         foreach ($proList as $k=>$pro) {
-        	foreach ($tags as $tag) {
-        		if($pro->tag_id == $tag->id){
-        			$proList[$k]->tag_name = $tag->tag_name;
-        		}
-        	}
-
-            foreach ($exps as $exp) { //拼接商品生存期
-                if($pro->product_id == $exp->id){
-                    $proList[$k]->exp = $exp->exp;
-                }
+            $pid = $pro->product_id;
+            if($type == 'sale'){ 
+                $num = $this->getNumOfSale($vmid,$pid); //计算即卖商品剩余数量
+                $pro->count = $num;      
+            }else{
+                $num = $this->getNumOfBook($vmid,$pid); //计算预定商品剩余数量
+                Log::debug('getNumOfBook---returns---'.$num);
+                $pro->count = $num;
             }
-            Cache::put('PRO_DETAIL_'.$proList[$k]->product_id.'_'.$vmId,$proList[$k],1440);
+
+            Cache::put('PRO_DETAIL_'.$proList[$k]->product_id.'_'.$vmid,$proList[$k],1440);
         }
-        Log::debug('MallService::showPros put in Cache---'.json_encode($proList));
+        Log::debug('MallService::showPros to '.$type.' put in Cache---'.json_encode($proList));
         return $proList;
     }
 
     // 获取某售货机下商品详情
-    // 参数1.pid 商品id 2.vmId
-    public function getProDetail($pid , $vmId){
-        if(empty($pid) || !isset($vmId)){
-            return 'error';
+    // 参数1.pid 商品id 2.vmId 3.type:'sale/book'
+    public function getProDetail($pid,$vmid,$type){
+        if(empty($pid) || !isset($vmid) || !isset($type)){
+            return 'parameter missing!';
         }
-        $proDetail = Cache::get('PRO_DETAIL_'.$pid.'_'.$vmId);
+        $proDetail = Cache::get('PRO_DETAIL_'.$pid.'_'.$vmid);
         Log::debug('MallService::getProDetail get data from Cache---'.json_encode($proDetail));
         if(empty($proDetail)){ //需要重新拉取售货机商品列表,放入缓存
-           $list = $this->showPros($vmId);
-           $proDetail = Cache::get('PRO_DETAIL_'.$pid.'_'.$vmId);
+           $list = $this->showPros($vmid,$type);
+           $proDetail = Cache::get('PRO_DETAIL_'.$pid.'_'.$vmid);
            Log::debug('MallService::getProDetail after showPros get data from Cache---'.json_encode($proDetail));
         }
         if(!empty($proDetail)){
@@ -64,6 +69,42 @@ class MallService{
         }else{
             return 'error';
         }
+    }
+
+    // 计算某台售货机下即卖商品剩余数量
+    public function getNumOfSale($vmid,$product_id){
+        $num = DB::table('skus')
+                        ->join('sku_supplys as skps','skus.id','=','skps.sku_id')
+                        ->where('skus.vm_id',$vmid)
+                        ->where('skus.product_id',$product_id)
+                        ->where('skps.status',2)
+                        ->count();
+        return $num;
+    }
+
+    // 计算某台售货机预定商品剩余数量
+    public function getNumOfBook($vmid,$product_id){
+        $skuList = DB::table('skus')
+                        ->where('vm_id',$vmid)
+                        ->where('product_id',$product_id)
+
+                        ->select('id','sku_size')
+                        ->get();
+        $max = 0;
+        foreach ($skuList as $sku) {
+            $max += $sku->sku_size;
+        }
+        
+        $existCount = DB::table('orders')
+                        ->join('order_details as ods','orders.id','=','ods.order_id')
+                        ->where('orders.channel',1)
+                        ->where('orders.orders_status','<>',3)
+                        ->where('orders.vmid',$vmid)
+                        ->where('ods.product_id',$product_id)
+                        ->count();
+        // dd($existCount);
+        $num = $max - $existCount;
+        return $num;
     }
 
     // 即卖生成单个取货码(下单后操作)
