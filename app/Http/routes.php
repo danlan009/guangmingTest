5dc3ceb383355bc302396182dc44b57097321bb4
liujinrong <liujinrong@ubox.cn>
2017-05-04 15:06:04 +0800

    route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -41,5 +41,9 @@ Route::get('task/updateImg',					'TaskController@updateImg'); // 检测图片是
 	Route::get('wx/cards', 						'MallController@wxCards');
 	Route::get('wx/account', 					'MallController@wxAccount');
 
+    Route::get('wx/ajax_prepay',                'PaymentController@ajaxPrepay');
+    Route::get('wx/notify_payment',             'PaymentController@notifyPayment');
+    Route::get('wx/test',                       'PaymentController@test');
+
 // });
 
