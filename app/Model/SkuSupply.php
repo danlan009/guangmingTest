295c0f209244b32ac11f6ab1d421c286d5d108f2
dongfanfan <13315478388@163.com>
2017-05-04 17:47:36 +0800

    update

---
+++ app/Model/SkuSupply.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SkuSupply extends Model
+{
+    protected $table = 'sku_supplys';
+    // public $timestamps = false;
+    protected $guarded = [];
+}
