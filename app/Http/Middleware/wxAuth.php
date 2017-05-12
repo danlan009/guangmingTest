2613cc2418e0b46b051a5fb1e1bb9a2e6fe7263f
dongfanfan <13315478388@163.com>
2017-05-12 15:11:03 +0800

    debug

--- app/Http/Middleware/wxAuth.php
+++ app/Http/Middleware/wxAuth.php
@@ -3,12 +3,15 @@ namespace App\Http\Middleware;
 use Illuminate\Http\Request;
 use Closure;
 
+use Log;
 use App\Model\User;
 class wxAuth{
 
 	public function handle($request, Closure $next){
 		// 微信鉴权
 		$oauthUser = session('wechat.oauth_user'); // esay-wechat自动配置session
+		// dd($oauthUser);
+		Log::debug('wxAuth---oauthUser returns---'.json_encode($oauthUser));
 		if(!empty($oauthUser)){
 			$openid = $oauthUser->getId(); // 获取openid
 			$nick_name = $oauthUser->getNickname();
@@ -21,7 +24,7 @@ class wxAuth{
 												'password' => $password
 											]
 										]);
-			\Log::debug('new user created ---'.json_encode($model));
+			Log::debug('new user created ---'.json_encode($model));
 			session(['openid',$openid]);
 		}
 		return $next($request);
