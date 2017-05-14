d923763a93106cdb36432c7c7685f6ae9f584b65
dongfanfan <13315478388@163.com>
2017-05-14 11:16:12 +0800

    test

--- app/Http/Controllers/SupplyController.php
+++ app/Http/Controllers/SupplyController.php
@@ -18,43 +18,12 @@ use Log;
 class SupplyController extends Controller 
 { 
     // 补货控制器   
-    public function test(){
-        // $phone = User::getPhone('qqqq');
-        // dd($phone);
-        // $mallService = new MallService();
-        // $vminfo = $mallService->getProDetail(100016,'0081008','book');
-        // $proList = $mallService->showPros('0081008','book');
-        // dd($proList);
-        // $json = json_encode($proList);
-        // return view('supply.test',['proList'=>$json]);
-        // $num = $mallService->getNumOfSale('0081008',100010);
-        // dd($num);
-        // $res = $mallService->singleBuyCode(10,20,100010,'0081008');
-        // dd($res);
- 
-        // $list = $mallService->getNumOfBook('0081008',100006);
-        // dd($list);
+    public function test(Application $wechat){
+        $userService = $wechat->user;
 
-        // $url = $mallService->getImg('products',100016,'l');
-        // dd($url);
-        // $supplyService = new SupplyService();
-        // $data = $supplyService->getSupplyData('0081008');
-        // dd($data);
-        // $orderService = new OrderService();
-        // $data = $orderService->handleOrdersToAllot('0081008');
-    
-        // $str = '{"1":{"product_id":100002,"product_name":"纯牛奶","sku_id":100062,"sku_size":10,"normal":3,"warn":0,"default_add":0,"actual_add":0},"2":{"product_id":100003,"product_name":"鲜牛奶","sku_id":100063,"sku_size":10,"normal":7,"warn":0,"default_add":3,"actual_add":3},"3":{"product_id":100002,"product_name":"光明酸奶","sku_id":100003,"sku_size":5,"normal":0,"warn":0,"default_add":0,"actual_add":0},"4":{"product_id":100002,"product_name":"光明酸奶","sku_id":100004,"sku_size":5,"normal":0,"warn":0,"default_add":0,"actual_add":0},"5":{"product_id":100002,"product_name":"光明酸奶","sku_id":100005,"sku_size":5,"normal":0,"warn":0,"default_add":0,"actual_add":0},"6":{"product_id":100006,"product_name":"光明鲜奶6","sku_id":100007,"sku_size":5,"normal":1,"warn":0,"default_add":2,"actual_add":2},"7":{"product_id":100007,"product_name":"光明鲜奶7","sku_id":100008,"sku_size":5,"normal":1,"warn":0,"default_add":1,"actual_add":1},"8":{"product_id":100008,"product_name":"光明鲜奶8","sku_id":100009,"sku_size":5,"normal":1,"warn":0,"default_add":1,"actual_add":1},"9":{"product_id":100009,"product_name":"光明鲜奶9","sku_id":100010,"sku_size":5,"normal":2,"warn":0,"default_add":2,"actual_add":2},"10":{"product_id":100010,"product_name":"光明鲜奶10","sku_id":100011,"sku_size":5,"normal":4,"warn":0,"default_add":1,"actual_add":1},"19":{"product_id":100001,"product_name":"光明鲜奶","sku_id":100064,"sku_size":5,"normal":5,"warn":0,"default_add":0,"actual_add":0}}';
-        // $data = json_decode($str,true);
-        // dd($data);
-
-       
-        // $this->finishSupply();
-        
-
-        // $this->getDateAfterWeekDays(30);
-
-
-        return view('supply.startSupplyment');
+        $user = $userService->get($openId);
+        echo $user->nickname;
+        // return view('supply.startSupplyment');
         // return phpinfo();
         // echo 111;
 
@@ -65,22 +34,22 @@ class SupplyController extends Controller
     {
         Log::info('request arrived.');
         $server = $wechat->server;
-        // $server->setMessageHandler(function($message){
-        //     Log::debug('Message:'.json_encode($message));
-        //     if(strtolower($message['MsgType']) == 'event') {
-        //         switch (strtolower($message->Event)) {
-        //             case 'location':
-        //                 $this->getLocation($message);
-        //                 break;
-        //             case 'click':
-        //                 return $this->sendMsg($message);
-        //                 break;
-        //             default:
-        //                 //return "Hello";
-        //                 break;
-        //         }
-        //     }
-        // });
+        $server->setMessageHandler(function($message){
+            Log::debug('Message:'.json_encode($message));
+            if(strtolower($message['MsgType']) == 'event') {
+                switch (strtolower($message->Event)) {
+                    case 'location':
+                        $this->getLocation($message);
+                        break;
+                    case 'click':
+                        return $this->sendMsg($message);
+                        break;
+                    default:
+                        //return "Hello";
+                        break;
+                }
+            }
+        });
 
         Log::info('return response.');
         $response  = $server->serve();
