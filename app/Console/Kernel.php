f23fbdc17e20b665c9e812e41ef9e10b4ba5bf91
dongfanfan <13315478388@163.com>
2017-05-24 17:13:52 +0800

    add a task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+use Log;
 class Kernel extends ConsoleKernel
 {
     /**
@@ -26,5 +27,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+		$schedule->call(function(){
+			Log::debug('schedule in---'.time());
+		})->everyMinute();
     }
 }
