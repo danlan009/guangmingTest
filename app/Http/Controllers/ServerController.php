2e2f31543148c617f7da6dae416e21df59551da4
dongfanfan <13315478388@163.com>
2017-06-29 14:18:28 +0800

    test

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -15,34 +15,19 @@ class ServerController extends Controller
     	Log::debug('Weixin message come in!');
     	$server = $app->server;
     	$server->setMessageHandler(function($message){
-    		// switch ($message->MsgType) {
-    		//         case 'event':
-    		//             return '收到事件消息';
-    		//             break;
-    		//         case 'text':
-    		//             return '收到文字消息';
-    		//             break;
-    		//         case 'image':
-    		//             return '收到图片消息';
-    		//             break;
-    		//         case 'voice':
-    		//             return '收到语音消息';
-    		//             break;
-    		//         case 'video':
-    		//             return '收到视频消息';
-    		//             break;
-    		//         case 'location':
-    		//             return '收到坐标消息';
-    		//             break;
-    		//         case 'link':
-    		//             return '收到链接消息';
-    		//             break;
-    		//         // ... 其它消息
-    		//         default:
-    		//             return '收到其它消息';
-    		//             break;
-    		//     }
-    		return 'hello';
+    		switch (strtolower($message->MsgType)) {
+                    case 'text':
+                        if(trim($message->Content) === '开始补货'){
+                          return 'gm.dev.uboxol.com/supply/start_supplyment';
+                            
+                        }
+                        break;
+                    
+                    
+                    default:
+                        return 'welcome!';
+                        break;
+                }
     	});
 
     	Log::debug('Message has returned to Weixin!');
