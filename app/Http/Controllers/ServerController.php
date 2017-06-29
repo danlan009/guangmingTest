fbdf84dff5dba451b4b8bf78dfeebc005d25ac9a
dongfanfan <13315478388@163.com>
2017-06-29 15:37:15 +0800

    test

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-use App\Http\Controllers\Controller;
-use Log;
+// use Log;
 
 use EasyWeChat\Foundation\Application;
 use EasyWeChat\Message\News;
