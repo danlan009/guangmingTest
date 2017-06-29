eea5e8b1e9010c2053ebaa96169168a5b202b56d
dongfanfan <13315478388@163.com>
2017-06-29 17:12:56 +0800

    test

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -101,6 +101,7 @@ class ServerController extends Controller
     public function authentication($openid){
         $json = json_decode(file_get_contents('sources/'.env('JSON_PATH').'/sender.json'),true);
         $openids = array_pluck('openid',$json);
+        Log::debug('ServerController-authentication---openids returns:'.json_encode($openids));
         if(in_array($openids,$openid)){
             return 1;
             
